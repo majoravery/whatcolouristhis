0da3ea221655d7a9465dd5d6c0be95487e42081a
coolsteed <vivianaverylim@gmail.com>
2015-08-11 16:39:02 +0800

    Update
    1. Large images resizes to fit
    2. Background changes colour for visibility

--- README.md
+++ README.md
@@ -1,18 +1,12 @@
 # whatcolouristhis
 
-#### What?
-whatcolouristhis is an eyedropper/colour picker written in Node.js. 
+#### About
+whatcolouristhis is an eyedropper/colour picker written in Node.js. Drag and drop or paste your image on the page and click anywhere on it to retrieve the colour information. Try it out <a href="http://britskit.github.io/whatcolouristhis/" target="_blank">here</a>.
 
 
-#### How?
-Drag and drop or paste your image on the page and click anywhere on it to retrieve the colour information.
+#### Compatibility
+Paste event is not compatible with Safari (yet).
 
 
-#### Where?
-Try it out <a href="http://britskit.github.io/whatcolouristhis/">here</a>.
-
-#### Why?
-Why not?
-
-#### License?
+#### License
 whatcolouristhis is licensed under the <a href="https://github.com/britskit/whatcolouristhis/blob/master/LICENSE.md">MIT License</a>.
--- dist/scripts.js
+++ dist/scripts.js
@@ -2,41 +2,18 @@
 var canvasPixelColor = require("canvas-pixel-color");
 
 var spaces = "&nbsp;&nbsp;&nbsp;";
-var dropbox, cvs, ctx, listener;
-
-dropbox = document.getElementById("dropbox");
-dropbox.addEventListener("paste", paste, false);
-
-dropbox.addEventListener("dragenter", dragenter, false);
-dropbox.addEventListener("dragover", dragover, false);
-dropbox.addEventListener("dragleave", dragleave, false);
-dropbox.addEventListener("drop", drop, false);
-
-canvasInit();
-
-cvs.addEventListener("click", function(ev) {
-	if (listener == true) {
-	  var hex = canvasPixelColor(ev, ctx)["hex"].toUpperCase();
-	  var rgba = canvasPixelColor(ev, ctx)["rgba"];
-	  if (rgba[3] == 0) {
-			document.getElementById("reply").innerHTML = "<span class=\"hex\">It is transparent.</span><br/>R: " + rgba[0] + spaces + "B: " + rgba[1] + spaces + "G: " + rgba[2] + spaces + "A: " + rgba[3];
-	  } 
-    else {
-	  	document.getElementById("reply").innerHTML = "<span class=\"hex\">It is <span style=\"color:" + hex + "\">" + hex + "</span></span>.<br/>R: " + rgba[0] + spaces + "B: " + rgba[1] + spaces + "G: " + rgba[2] + spaces + "A: " + rgba[3];
-	  }
-	}
-}, false)
+var dropbox, dummy, cvs, ctx, listener;
 
 function canvasInit() {
-	cvs = document.getElementById("canvas");
-	ctx = cvs.getContext("2d");
-	listener = false;
+  cvs = document.getElementById("canvas");
+  ctx = cvs.getContext("2d");
+  listener = false;
 }
 
 function dragenter(e) {
-  document.getElementById("intro").className = document.getElementById("intro").className.replace(/(?:^|\s)introMsg(?!\S)/g, 'uploadMsg');
   e.stopPropagation();
   e.preventDefault();
+  document.getElementById("intro").className = document.getElementById("intro").className.replace(/(?:^|\s)introMsg(?!\S)/g, 'uploadMsg');
 }
 
 function dragover(e) {
@@ -45,9 +22,9 @@ function dragover(e) {
 }
 
 function dragleave(e) {
-  document.getElementById("intro").className = document.getElementById("intro").className.replace(/(?:^|\s)uploadMsg(?!\S)/g, 'introMsg');
   e.stopPropagation();
   e.preventDefault();
+  document.getElementById("intro").className = document.getElementById("intro").className.replace(/(?:^|\s)uploadMsg(?!\S)/g, 'introMsg');
 }
 
 function drop(e) {
@@ -63,6 +40,7 @@ function drop(e) {
 }
 
 function paste(e) {
+  console.log("paste!");
   e.stopPropagation();
   e.preventDefault();
 
@@ -77,9 +55,9 @@ function dropFile(files) {
   var imageType = /^image\//;
   
   if (!imageType.test(files[0].type))
-  	invalidMsg();
+    invalidMsg();
   else
-		createImage(files[0]);
+    createImage(files[0]);
 }
 
 function pasteFile(files) {
@@ -96,11 +74,23 @@ function pasteFile(files) {
 
 function createImage(image) {
   var img = document.createElement("img");
+  window.URL = window.URL || window.webkitURL;
   img.src = window.URL.createObjectURL(image);
-  img.onload = function() {
+  img.onload = function() {        
+    //resizing large images
+    if (img.width > 800) {
+      img.height = Math.round(800 / img.width * img.height);
+      img.width = 800;
+    }
+    else if (img.height > 800) {
+      img.width = Math.round(800 / img.height * img.width);
+      img.height = 800;
+    }
+
+    //drawing the image on canvas
     canvas.width = img.width;
     canvas.height = img.height;
-    ctx.drawImage(img, 0, 0);
+    ctx.drawImage(img, 0, 0, img.width, img.height);
     listener = true;
     window.URL.revokeObjectURL(this.src);
   }
@@ -110,6 +100,33 @@ function invalidMsg() {
   canvas.width, canvas.height = 0;
   document.getElementById("reply").innerHTML = "<span class=\"hex\">This is not a valid image file.</span>";
 }
+
+dropbox = document.getElementById("dropbox");
+dropbox.addEventListener("paste", paste, false);
+dropbox.addEventListener("dragenter", dragenter, false);
+dropbox.addEventListener("dragover", dragover, false);
+dropbox.addEventListener("dragleave", dragleave, false);
+dropbox.addEventListener("drop", drop, false);
+
+
+canvasInit();
+cvs.addEventListener("click", function(ev) {
+  if (listener == true) {
+    var hex = canvasPixelColor(ev, ctx)["hex"].toUpperCase();
+    var rgba = canvasPixelColor(ev, ctx)["rgba"];    
+    if (rgba[3] == 0) {
+      document.body.style.background = "#FAFAFA";
+      document.getElementById("reply").innerHTML = "<span class=\"hex\">It is transparent.</span><br/>R: " + rgba[0] + spaces + "B: " + rgba[1] + spaces + "G: " + rgba[2] + spaces + "A: " + rgba[3];
+    }
+    else {
+      if (rgba[0] > 245 && rgba[1] > 245 && rgba[2] > 245)
+        document.body.style.background = "#C8C8C8";
+      else 
+        document.body.style.background = "#FAFAFA";
+      document.getElementById("reply").innerHTML = "<span class=\"hex\">It is <span style=\"color:" + hex + "\">" + hex + "</span></span>.<br/>R: " + rgba[0] + spaces + "B: " + rgba[1] + spaces + "G: " + rgba[2] + spaces + "A: " + rgba[3]; 
+    }
+  }
+}, false)
 },{"canvas-pixel-color":2}],2:[function(require,module,exports){
 function canvasPixelColor(ev, context) {
   var x = ev.offsetX || ev.layerX;
